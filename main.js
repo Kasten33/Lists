cf5ec74517ff126868fafa166b2ab452dad92386
Kasten33 <hurlrykaden@gmail.com>
2023-10-11 17:39:07 -0600

    the start

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 productArray = [];
+
+
+
+
 function addProduct() {
-
-   
-    let input = document.getElementById("input").value;
-
-
-
-
-    
-    
-    
-    
+    let input = document.getElementById("product").value;
+    let output = document.getElementById("product");
+    const dive = document.createElement("div");
+    dive.innerHTML = output;
+    document.body.appendChild(dive);
     let newProduct = { input };
+     for (let i = 0; i < productArray.length; i++){
+      
+      input 
+      dive.textContent = `${productArray[i].input}`   
+      output.appendChild(dive)
+    }
+   
+
+
+     
+    productArray.push(newProduct)
+    console.log(productArray)
+    
+    
+    
 
 
 
