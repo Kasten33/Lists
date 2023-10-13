83be51618eada2a50bed0cdbc8802a7750654887
Kasten33 <hurlrykaden@gmail.com>
2023-10-13 12:18:59 -0600

    got it

--- main.js
+++ main.js
@@ -7,20 +7,24 @@ function addProduct() {
     let input = document.getElementById("product").value;
     let output = document.getElementById("product");
     const dive = document.createElement("div");
-    dive.innerHTML = output;
-    document.body.appendChild(dive);
+    dive.classList.add('container2')
+    dive.innerText = output;
     let newProduct = { input };
+
+    productArray.push(newProduct)
+
      for (let i = 0; i < productArray.length; i++){
-      
-      input 
-      dive.textContent = `${productArray[i].input}`   
-      output.appendChild(dive)
+ 
+      dive.style.display = "block";
+      dive.textContent = `${productArray[i].input}`;
+      output.appendChild(dive);
+      document.body.appendChild(dive);
     }
    
 
 
      
-    productArray.push(newProduct)
+   
     console.log(productArray)
     
     
